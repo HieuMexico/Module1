0370775d3c9034d5540489f5df198103c5a60967
unknown <lidepzai75@gmail.com>
2023-02-18 20:22:20 +0700

    th

--- TH_BT_Tren_Lop/for.js
+++ TH_BT_Tren_Lop/for.js
@@ -1,6 +1,6 @@
-for (let i = 1; i <= 10; i ++) {
-    console.log(i);
-}
+//for (let i = 1; i <= 10; i ++) {
+//    console.log(i);
+//}
 //B1 : let i = 1
 //B2 : ktra i co <= 10 hay k
 //B3 : console.log(i) : in i ra
